3870ec1ab492880ceb020e04120c892d10823043
EdGruberman <ed@rjump.com>
2012-04-05 23:09:49 -0700

    Initial release

---
+++ src/edgruberman/bukkit/silkpoke/Vaporizer.java
@@ -0,0 +1,34 @@
+package edgruberman.bukkit.silkpoke;
+
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.World.Environment;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Immediately removes any water in the nether that is the result of a broken ice block.
+ */
+public class Vaporizer implements Listener {
+
+    Vaporizer(final Plugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockBreak(final BlockBreakEvent event) {
+        if (event.getBlock().getWorld().getEnvironment() != Environment.NETHER) return;
+
+        if (event.getBlock().getTypeId() != Material.ICE.getId()) return;
+
+        if (event.getPlayer().hasPermission("silktouch.nether.water")) return;
+
+        event.getBlock().setTypeIdAndData(Material.AIR.getId(), (byte) 0, true);
+        event.getBlock().getWorld().playEffect(event.getBlock().getLocation(), Effect.EXTINGUISH, 0);
+        event.getBlock().getWorld().playEffect(event.getBlock().getLocation(), Effect.SMOKE, BlockFace.UP);
+    }
+
+}
